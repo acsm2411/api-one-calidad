39d23157ec549f374723b7b378c31b0a0dc637de
Andres Silva <Andres.Silva@anuntatech.com>
2022-08-20 19:42:51 -0500

    Test No Content

--- main_test.py
+++ main_test.py
@@ -7,3 +7,7 @@ client = TestClient(app)
 def test_index_route():
     response = client.get('/infoUsers/Roslyn.Rempel')
     assert response.status_code == 200
+
+def test_index_route_no_content():
+    response = client.get('/infoUsers/test')
+    assert response.status_code == 204
