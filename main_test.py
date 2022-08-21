8999efb77df114fcfe3a0be47e712b7a0ba980f5
Andres Silva <Andres.Silva@anuntatech.com>
2022-08-21 05:36:06 -0500

    Tests name change

--- main_test.py
+++ main_test.py
@@ -4,10 +4,10 @@ from main import app
 
 client = TestClient(app)
 
-def test_index_route():
+def test_route():
     response = client.get('/infoUsers/Roslyn.Rempel')
     assert response.status_code == 200
 
-def test_index_route_no_content():
+def test_route_no_content():
     response = client.get('/infoUsers/test')
     assert response.status_code == 204
